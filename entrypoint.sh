60fac4e838af868a57fc86fb1b592bd3508b9a53
Lukas Pawlik <lukas.pawlik@10up.com>
2023-04-26 22:30:47 +0200

    Update base image

--- entrypoint.sh
+++ entrypoint.sh
@@ -28,6 +28,10 @@ chmod 644 "$KNOWN_HOSTS_PATH"
 chmod 600 "$WPENGINE_SSH_KEY_PRIVATE_PATH"
 chmod 644 "$WPENGINE_SSH_KEY_PUBLIC_PATH"
 
+echo "Step 0"
 git config core.sshCommand "ssh -i $WPENGINE_SSH_KEY_PRIVATE_PATH -o UserKnownHostsFile=$KNOWN_HOSTS_PATH"
+echo "Step 1"
 git remote add $WPENGINE_ENV git@$WPENGINE_HOST:$WPENGINE_ENV/$WPENGINE_ENVIRONMENT_NAME.git
+echo "Step 2"
 git push -fu $WPENGINE_ENV $BRANCH:master
+echo "Step 3"
